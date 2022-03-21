3ceb9d25ff706dcee32c4ce8e87bcc76afdea564
it21170034 Shabina Farveen <99266981+it21170034@users.noreply.github.com>
2022-03-21 12:23:45 +0000

    exercise 1 done

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,17 @@
 #include "Student.h"
 #include <iostream>
+#include<cstring>
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
-  
-}
+void Student::assignDetails(int stdId, char namee[]) {
+  studentId = stdId;
+
+  strcpy(name, namee);
+};
 
 // Display StudentId and Name
-Student::display() {
-  
-}
+void Student::display() {
+  cout << Student::studentId << endl;
+  cout << Student::name << endl;
+};
